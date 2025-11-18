b2ff336299b2821b8802bb62f69174db700e8bbc
LMellaVillanueva <mellalucas.v@gmail.com>
2025-11-18 20:05:15 -0300

    deploy config

--- server.py
+++ server.py
@@ -4,18 +4,19 @@ from app.config.db_connection import connectToMySQL
 import os
 
 def create_app():
-    CORS(app, resources={r"/*": {"origins": os.getenv('ORIGIN')}}, supports_credentials=True) 
+    CORS(app, resources={r"/*": {"origins": os.getenv('ORIGIN')}}, supports_credentials=True)
 
     from app.controllers.users import user_bp
     from app.controllers.vouchers import voucher_bp
     app.register_blueprint(user_bp, url_prefix='/user')
     app.register_blueprint(voucher_bp, url_prefix='/voucher')
+
     return app
 
-create_app()
+app = create_app()
 
-print("✅ Conectado a MySQL") if connectToMySQL().query_db("SELECT 1") else print("❌ Error MySQL")
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+try:
+    connectToMySQL().query_db("SELECT 1")
+    print("✅ Conectado a MySQL")
+except:
+    print("❌ Error MySQL")
