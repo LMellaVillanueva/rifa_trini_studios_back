16895fe6fb820d329b87e0ee37d2b6ad29dd6762
LMellaVillanueva <mellalucas.v@gmail.com>
2025-11-19 21:05:24 -0300

    deploy config fixed

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 import os
 
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
 def create_app():
     app = Flask(__name__);
     app.secret_key = os.getenv('SECRET_KEY')
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_KEY')
-    jwt = JWTManager(app)
-    bcrypt = Bcrypt(app)
+    # Inicializar extensiones con la app
+    bcrypt.init_app = Bcrypt(app)
+    jwt.init_app = JWTManager(app)
     return app
